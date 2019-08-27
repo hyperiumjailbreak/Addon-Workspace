f2aeae7db8c13ed2b0d1635c80df0230bdf83082
Reece Dunham <me@rdil.rocks>
2019-08-27 14:30:38 -0400

    Update example

--- src/main/java/com/example/AddonExample.java
+++ src/main/java/com/example/AddonExample.java
@@ -4,31 +4,24 @@ import cc.hyperium.event.ChatEvent;
 import cc.hyperium.event.EventBus;
 import cc.hyperium.event.InvokeEvent;
 import cc.hyperium.internal.addons.IAddon;
-
 import net.minecraft.client.Minecraft;
 
 public class AddonExample implements IAddon {
-
-    @Override
-    public void onLoad() {
+    @Override public void onLoad() {
+        // boot addon
         System.out.println("Successfully loaded Addon!");
-        
+
         EventBus.INSTANCE.register(this);
     }
 
-    @InvokeEvent
-    private void onChatMessage(ChatEvent event) {
+    @InvokeEvent private void onChatMessage(ChatEvent event) {
         if (event.getChat().getUnformattedText().contains("secret message!")) {
             Minecraft.getMinecraft().toggleFullscreen();
         }
     }
 
-    @Override
-    public void onClose() {
-        System.out.println("Closing...");
-    }
-    
-    @Override
-    public void sendDebugInfo() {
+    @Override public void onClose() {
+        // shutdown the addon
+        EventBus.INSTANCE.unregister(this);
     }
 }
