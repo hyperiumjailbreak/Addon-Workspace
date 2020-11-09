a40abbc2ddccc1f2153a1839a1c9898d3965eabb
Reece Dunham <me@rdil.rocks>
2020-11-09 15:17:09 -0500

    Style fixes

--- src/main/java/com/example/AddonExample.java
+++ src/main/java/com/example/AddonExample.java
@@ -7,20 +7,23 @@ import cc.hyperium.internal.addons.IAddon;
 import net.minecraft.client.Minecraft;
 
 public class AddonExample implements IAddon {
-    @Override public void onLoad() {
+    @Override
+    public void onLoad() {
         // boot addon
         System.out.println("Successfully loaded Addon!");
 
         EventBus.INSTANCE.register(this);
     }
 
-    @InvokeEvent private void onChatMessage(ChatEvent event) {
+    @InvokeEvent
+    private void onChatMessage(ChatEvent event) {
         if (event.getChat().getUnformattedText().contains("secret message!")) {
             Minecraft.getMinecraft().toggleFullscreen();
         }
     }
 
-    @Override public void onClose() {
+    @Override
+    public void onClose() {
         // shutdown the addon
         EventBus.INSTANCE.unregister(this);
     }
